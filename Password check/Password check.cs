782c88526af8e8ba2c582b9ee123826d9d72f295
GEM <41048774+Ura955@users.noreply.github.com>
2021-09-10 19:14:14 +0800

    Add files via upload

---
+++ Password check/Password check.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Password_check
+{
+    class Password_Check
+    {     
+        public static void Main()
+        {
+            //Константа пароля
+            const string passwordExemple = "PASSWORD";
+            //Переменная пароля введенного в командной строке
+            string password;
+
+            //Ввод пароля в командную страку и присвоение его переменной password
+            Console.Write("Введите пароль: ");
+            password = Console.ReadLine();
+
+            //Сравнение введенного пароля с константой
+            if (password == passwordExemple)
+            {
+                Console.WriteLine("Пароль подтвержден!");
+            }
+            else
+            {
+                Console.WriteLine("Неверный пароль!");
+            }
+
+            //Ожидание ввода для завершения программы
+            Console.ReadKey();
+        }
+    }
+}
